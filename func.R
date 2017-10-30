62f2f1f1d1bfada521a17d7ae6ec83d889a0af39
Murilo Salgado Razoli <ra150987@IME.UNICAMP.BR>
2017-10-30 11:02:58 -0200

    Fun

--- func.R
+++ func.R
@@ -9,3 +9,55 @@ ipak <- function(pkg){
 cv <- function(x){
   100* sd(x)/mean(x)
 }
+
+cl <- function(x){
+  y = vector("integer", length(x))
+  for(i in 1:length(x)){
+    if(x[i] <= 4){
+      y[i] <- "Baixo"
+    }
+    else if(x[i] > 4 & x[i] <= 7){
+      y[i] <- "Médio"
+    }
+    else
+      y[i] <- "Alto"
+  }
+return(factor(y))
+}
+
+
+corr <- function(m.X){
+# Fornecer
+
+# m.X : tabela de contingência
+
+# Saída
+
+# v.gamma : valores singulares > 0
+# inercia : inercia associada a cada componente
+
+# Tabela de contingência
+m.X <- rbind(cbind(135, 140, 95, 55, 40, 60),cbind(50, 115, 40, 60, 5, 15),cbind(90, 55, 20, 35, 40, 10),cbind(60, 25, 35, 10, 5, 30),cbind(30, 20, 5, 10, 10 ,20))
+dimnames(m.X) <- list(c("Sony","Aiwa","Gradiente","Philips","Sharp"),c("Qualidade","Tecnologia","Potência","Recursos","Preço","Confiança"))
+names(dimnames(m.X)) <- c("Marca","                  Atributo")
+
+# teste de chi-quadrado
+chisq.test(m.X)
+
+# Inércia
+resultCA <- ca(m.X) # names(resultCA)
+inercia<-summary(resultCA)$scree # names(summary(resultCA))
+
+# Componentes
+resultFCA <- plot(resultCA,xlab="componente 1",ylab="componente 2",cex=1.2)
+xtable(resultFCA$rows,digits=4)
+xtable(resultFCA$cols,digits=4)
+biplot(resultFCA$rows,resultFCA$cols,var.axes=FALSE,xlab="componente 1", ylab="componente 2",cex=1.2,xlim=c(-0.9,0.5))
+abline(0,0,lty=2)
+abline(v=0,lty=2)
+
+
+}
+
+
+
